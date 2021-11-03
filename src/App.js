187a5fb33964ca7bf8c462ec5031ae43248bedfd
AbbalYouness <abbal.youness@gmail.com>
2021-11-03 17:50:59 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div class="attribution">
+        Challenge by{" "}
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Your Name Here</a>.
+      </div>
+    </div>
+  );
+}
+
+export default App;
